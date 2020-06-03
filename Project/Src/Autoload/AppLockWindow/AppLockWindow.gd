d804c99ad218cfa1594c00c71c4ef1d9d10b926f
Pheeranut First <plotrunn343@gmail.com>
2020-06-03 19:19:51 +0700

    Add verify system

--- Project/Src/Autoload/AppLockWindow/AppLockWindow.gd
+++ Project/Src/Autoload/AppLockWindow/AppLockWindow.gd
@@ -19,11 +19,14 @@ extends Node
 #      Constants
 #-------------------------------------------------
 
+const VERIFY_DELAY_TIME = 0.8
+
 #-------------------------------------------------
 #      Properties
 #-------------------------------------------------
 
 onready var ask_code_label = $CanvasLayer/MarginContainer/Control/VBox/AskCodeHBox/AskCodeLabel
+onready var pwd_line_edit = $CanvasLayer/MarginContainer/Control/VBox/PasswordLineEdit
 
 var my_unique_code = "123456789"
 
@@ -46,12 +49,21 @@ func _ready() -> void:
 #      Public Methods
 #-------------------------------------------------
 
+func verify():
+	if not _is_code_match():
+		print("NOT MATCH!")
+	else:
+		print('wdf hacker')
+
 #-------------------------------------------------
 #      Connections
 #-------------------------------------------------
 
 func _on_CopyLinkButton_pressed() -> void:
 	OS.set_clipboard(ask_code_label.text)
+
+func _on_VerifyButton_pressed() -> void:
+	verify()
 
 #-------------------------------------------------
 #      Private Methods
@@ -61,7 +73,10 @@ func _update_request_code():
 	var ahl = AppHashLock.new()
 	ask_code_label.set_text(ahl.get_request_code(my_unique_code))
 
+func _is_code_match() -> bool:
+	var ahl = AppHashLock.new()
+	return ahl.is_match(pwd_line_edit.text, my_unique_code)
+
 #-------------------------------------------------
 #      Setters & Getters
 #-------------------------------------------------
-
