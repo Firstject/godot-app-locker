fd6445947e1c8e92b59fea6d1df79a7f35722c84
Pheeranut First <plotrunn343@gmail.com>
2020-06-05 01:15:42 +0700

    Add configurable app unique id (game owner)

--- Project/Src/Scene/CodeDevGenerator/CodeDevGenerator.gd
+++ Project/Src/Scene/CodeDevGenerator/CodeDevGenerator.gd
@@ -25,12 +25,17 @@ extends Node
 
 onready var request_line_edit = $MarginContainer/Control/VBox/RequestLineEdit
 onready var output_label = $MarginContainer/Control/VBox/OutputHbox/OutputLabel
+onready var unique_id_panel = $MarginContainer/Control/SettingsToggleButton/UniqueIDPanel
+onready var unique_id_line_edit = $MarginContainer/Control/SettingsToggleButton/UniqueIDPanel/VBox/UniqueLineEdit
 
 export (String) var unique_app_id = "123456789"
 
 #-------------------------------------------------
 #      Notifications
 #-------------------------------------------------
+
+func _ready() -> void:
+	_update_unique_id_setting_text()
 
 #-------------------------------------------------
 #      Virtual Methods
@@ -58,9 +63,24 @@ func _on_GenerateButton_pressed() -> void:
 func _on_CopyLinkButton_pressed() -> void:
 	OS.set_clipboard(output_label.text)
 
+func _on_SettingsToggleButton_pressed() -> void:
+	_toggle_unique_id_panel_visible()
+
+func _on_UniqueLineEdit_text_changed(new_text: String) -> void:
+	_update_unique_id_setting(new_text)
+
 #-------------------------------------------------
 #      Private Methods
 #-------------------------------------------------
+
+func _toggle_unique_id_panel_visible():
+	unique_id_panel.visible = !unique_id_panel.visible
+
+func _update_unique_id_setting_text():
+	unique_id_line_edit.text = unique_app_id
+
+func _update_unique_id_setting(new_text: String):
+	unique_app_id = new_text
 
 #-------------------------------------------------
 #      Setters & Getters
